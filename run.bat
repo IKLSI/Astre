cca447ae02aa3f108f50b658471495f10fbf63fa
Lorenzo De Macedo <lorenzo.demacedo@outlook.fr>
2023-12-12 15:17:47 +0100

    mise a jour des .bat

--- run.bat
+++ run.bat
@@ -1,12 +1,12 @@
 @echo off
-set FX_PATH=.\lib\javafx-sdk-17.0.9\lib
+set FX_PATH=.\lib\javafx-sdk-21.0.1\lib
 
 javac --module-path "%FX_PATH%" --add-modules javafx.controls,javafx.fxml -encoding utf8 "@compile.list" -d .\bin
 
 cd bin
 
-set FX_PATH=..\lib\javafx-sdk-17.0.9\lib
+set FX_PATH=..\lib\javafx-sdk-21.0.1\lib
 
-java --module-path "%FX_PATH%" --add-modules javafx.controls,javafx.fxml Essai
+java --module-path "%FX_PATH%" --add-modules javafx.controls,javafx.fxml App
 
 exit /b 0
