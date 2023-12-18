e8c385269104eb9a6f727372b10d8e9f57c4362a
Langlois Lucas <55955180+Scarlet76@users.noreply.github.com>
2023-12-18 12:30:26 +0100

    add nbH

--- src/metier/Affectation.java
+++ src/metier/Affectation.java
@@ -2,30 +2,51 @@ package metier;
 
 public class Affectation
 {
+	private String codMod;
+	private int codInter;
+	private int codCatHeure;
+	private String commentaire;
 	private String nom;
 	private String type;
 	private int nbSem;
-	private int nbGp;
+	private int nbGrp;
 	private int totalEqTd;
+	private int nbH;
 
-	public Affectation(String nom, String type, int nbSem, int nbGp, int totalEqTd)
+	public Affectation(String codMod, int codInter, int codCatHeure, String commentaire, String nom, String type, int nbSem, int nbGrp, int totalEqTd, int nbH)
 	{
+		this.codMod = codMod;
+		this.codInter = codInter;
+		this.codCatHeure = codCatHeure;
+		this.commentaire = commentaire;
 		this.nom = nom;
 		this.type = type;
 		this.nbSem = nbSem;
-		this.nbGp = nbGp;
+		this.nbGrp = nbGrp;
 		this.totalEqTd = totalEqTd;
+		this.nbH = nbH;
 	}
 
-	public String getNom() { return nom; }
-	public String getType() { return type; }
-	public int getNbSem() { return nbSem; }
-	public int getNbGp() { return nbGp; }
-	public int getTotalEqTd() { return totalEqTd; }
+	public String getNom() { return this.nom; }
+	public String getType() { return this.type; }
+	public int getNbSem() { return this.nbSem; }
+	public int getNbGrp() { return this.nbGrp; }
+	public int getTotalEqTd() { return this.totalEqTd; }
+	public String getCodMod() { return this.codMod; }
+	public int getCodInter() { return this.codInter; }
+	public int getCodCatHeure() { return this.codCatHeure; }
+	public String getCommentaire() { return this.commentaire; }
+	public int getNbH() { return this.nbH; }
 
 	public void setNom(String nom) { this.nom = nom; }
 	public void setType(String type) { this.type = type; }
 	public void setNbSem(int nbSem) { this.nbSem = nbSem; }
-	public void setNbGp(int nbGp) { this.nbGp = nbGp; }
+	public void setNbGrp(int nbGrp) { this.nbGrp = nbGrp; }
 	public void setTotalEqTd(int totalEqTd) { this.totalEqTd = totalEqTd; }
+	public void setCodMod(String codMod) { this.codMod = codMod; }
+	public void setCodInter(int codInter) { this.codInter = codInter; }
+	public void setCodCatHeure(int codCatHeure) { this.codCatHeure = codCatHeure; }
+	public void setCommentaire(String commentaire) { this.commentaire = commentaire; }
+	public void setNbH(int nbH) { this.nbH = nbH; }
+
 }
