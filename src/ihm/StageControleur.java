c5b3d0b7c9c809ae3c4c947bab07a6d5aeec48de
KLS <100925067+IKLSI@users.noreply.github.com>
2024-01-12 13:07:39 +0100

    Modification StageControleur.java

--- src/ihm/StageControleur.java
+++ src/ihm/StageControleur.java
@@ -254,7 +254,7 @@ public class StageControleur implements Initializable
 			Controleur.insertAffectationRessource(new Affectation(
 				codes,
 				Controleur.getCodInter(affectation.getNom()).get(0),
-				Controleur.getCodCatInter(affectation.getType()),
+				Controleur.getCodCatHeure(affectation.getType()),
 				affectation.getCommentaire(),
 				affectation.getNom(),
 				affectation.getType(),
